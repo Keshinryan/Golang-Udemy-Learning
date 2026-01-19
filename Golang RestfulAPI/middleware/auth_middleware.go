0aed589b76730cfd2d48b6e223818073ac766ef5
Jason Patrick <108507438+Keshinryan@users.noreply.github.com>
2026-01-19 10:53:52 +0700

    first commit

--- Golang RestfulAPI/middleware/auth_middleware.go
+++ Golang RestfulAPI/middleware/auth_middleware.go
@@ -10,14 +10,14 @@ type AuthMiddleware struct {
 	Handler http.Handler
 }
 
-func NewAuthMiddleware(handler http.Handler) *AuthMiddleware{
+func NewAuthMiddleware(handler http.Handler) *AuthMiddleware {
 	return &AuthMiddleware{Handler: handler}
 }
 
-func (middleware *AuthMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request){
-	if "RAHASIA" == request.Header.Get("x-API-Key"){
-		middleware.Handler.ServeHTTP(writer,request)
-	}else{
+func (middleware *AuthMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	if "RAHASIA" == request.Header.Get("x-API-Key") {
+		middleware.Handler.ServeHTTP(writer, request)
+	} else {
 		writer.Header().Set("Content-Type", "application/json")
 		writer.WriteHeader(http.StatusUnauthorized)
 
